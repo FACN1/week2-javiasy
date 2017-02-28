c6a4f2dd819fefd568d81de31c56909bdf01442c
joudy <hilbertbonamy@gmail.com>
2017-02-28 13:11:43 +0200

    todoadd now chan ges the id

--- todo.js
+++ todo.js
@@ -25,6 +25,7 @@ var todoFunctions = {
         // return a new array, it should contain todos with the newTodo added to the end.
         // add an id to the newTodo. You can use the generateId function to create an id.
         // hint: array.concat
+        newTodo.id = this.generateId();
         return todos.concat(newTodo);
     },
     deleteTodo: function (todos, idToDelete) {
