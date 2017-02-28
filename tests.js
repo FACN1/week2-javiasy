124a7b74b6fa41b470682f2fe3064db10deecbdc
Macintosh Helper <noreply@macintoshhelper.com>
2017-02-28 12:31:30 +0200

    Add initial markTodo tests

--- tests.js
+++ tests.js
@@ -1,18 +1,5 @@
 QUnit.test( "addTodo function", function( assert ) {
-    // var result = (1, 1); //no change/coins just an empty array
-    // var expected = [];
-    // assert.deepEqual(result, expected);
-
-
-    var result = todo.todoFunctions.addTodo(todo.state,{id: 0,description: 'fourth todo'});
-    var expected =[
-        { id: -3, description: 'first todo'},
-        { id: -2, description: 'second todo'},
-        { id: -1, description: 'third todo'},
-        { id: 0, description: 'fourth todo'}
-    ];
-
-    assert.deepEqual(result, expected, "addTodo passed!" );
+    assert.equal(/*input*/1,/*output*/ 1, "addTodo passed!" );
 });
 
 QUnit.test("deleteTodo function", function(assert) {
@@ -40,6 +27,47 @@ QUnit.test("deleteTodo function", function(assert) {
     assert.deepEqual(deleter(input, -3), output2, "Removed the third todo (id: -3) from todoList" );
     assert.deepEqual(deleter(input, 1), input, "No id should return the same" );
     assert.deepEqual(deleter(badinput, 1), badinput, "Test for no id element" );
+
+
+
+    /*
+    Expected input:
+        todos -> [todo0, todo1, todo2]
+        todo -> [id, description, done]
+        types -> [int, string, boolean]
+
+        [todo0, todo1, todo2]
+    Expected output:
+        [todo0, todo1]
+    */
+});
+
+QUnit.test("markTodo function", function(assert) {
+    var input1 = [
+        { id: -3, description: 'first todo'},
+        { id: -2, description: 'second todo'},
+        { id: -1, description: 'third todo'}
+    ];
+    var output1 = [
+        { id: -3, description: 'first todo', done: false},
+        { id: -2, description: 'second todo', done: false},
+        { id: -1, description: 'third todo', done: true}
+    ];
+
+    // var output2 = [
+    //    { id: -2, description: 'second todo'},
+    //    { id: -1, description: 'third todo'}
+    // ];
+
+    // var badinput = [1,3,4]
+
+    var marker = todo.todoFunctions.markTodo;
+
+
+    assert.deepEqual(marker(input1, -1), output1, "Removed the third todo (id: -1) from todoList" );
+    // assert.deepEqual(market(input1, -3), output2, "Removed the third todo (id: -3) from todoList" );
+    // assert.deepEqual(marker(input, 1), input, "No id should return the same" );
+    // assert.deepEqual(marker(badinput, 1), badinput, "Test for no id element" );
 
 
 
