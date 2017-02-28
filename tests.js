4267a7288f19c31d843aef4ce891117e63ba6e25
Matt Davis <matthewpeterdavis@gmail.com>
2017-02-28 12:04:39 +0200

    Pass second test and add more edge cases

--- tests.js
+++ tests.js
@@ -18,9 +18,15 @@ QUnit.test("deleteTodo function", function(assert) {
        { id: -1, description: 'third todo'}
     ];
 
+    var badinput = [1,3,4]
 
-    assert.deepEqual(todo.todoFunctions.deleteTodo(input, -1), output1, "Removed the third todo (id: -1) from todoList" );
-    assert.deepEqual(todo.todoFunctions.deleteTodo(input, -3), output2, "Removed the third todo (id: -3) from todoList" );
+    var deleter = todo.todoFunctions.deleteTodo;
+
+
+    assert.deepEqual(deleter(input, -1), output1, "Removed the third todo (id: -1) from todoList" );
+    assert.deepEqual(deleter(input, -3), output2, "Removed the third todo (id: -3) from todoList" );
+    assert.deepEqual(deleter(input, 1), input, "No id should return the same" );
+    assert.deepEqual(deleter(badinput, 1), badinput, "Test for no id element" );
 
 
 
