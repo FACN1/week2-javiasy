bc8363d9e80818f81dde986acd59f765c26b160c
Matt Davis <matthewpeterdavis@gmail.com>
2017-03-01 10:03:11 +0200

    add internal variable for test, fixes #18

--- tests.js
+++ tests.js
@@ -1,5 +1,11 @@
 QUnit.test( "addTodo function", function( assert ) {
-    var result = todo.todoFunctions.addTodo(todo.state,{id: 0,description: 'fourth todo'});
+    var testtodos = [
+      { id: -3, description: 'first todo'},
+      { id: -2, description: 'second todo'},
+      { id: -1, description: 'third todo'}
+    ];
+
+    var result = todo.todoFunctions.addTodo(testtodos,{id: 0,description: 'fourth todo'});
     var expected =[
         { id: -3, description: 'first todo'},
         { id: -2, description: 'second todo'},
@@ -7,14 +13,14 @@ QUnit.test( "addTodo function", function( assert ) {
         { id: 0, description: 'fourth todo'}
     ];
 
-    var result2 = todo.todoFunctions.addTodo(todo.state,{id: null, description: "fourth todo"})
+    var result2 = todo.todoFunctions.addTodo(testtodos,{id: null, description: "fourth todo"})
     var expected2 =[
         { id: -3, description: 'first todo'},
         { id: -2, description: 'second todo'},
         { id: -1, description: 'third todo'},
         { id: 1, description: 'fourth todo'}
     ];
-    var result3 = todo.todoFunctions.addTodo(todo.state,{description: "fourth todo"})
+    var result3 = todo.todoFunctions.addTodo(testtodos,{description: "fourth todo"})
     var expected3 =[
         { id: -3, description: 'first todo'},
         { id: -2, description: 'second todo'},
